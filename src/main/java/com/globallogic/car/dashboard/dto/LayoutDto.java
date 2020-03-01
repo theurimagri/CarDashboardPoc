875272c28e4166a6e12f8f4ee7e2933f31274ba9
Vinicius Antonio Gai <viniciustoni@gmail.com>
2020-03-01 22:26:29 +0100

    - Update add template layout to add files; - Creating new endpoint to load layout template

--- src/main/java/com/globallogic/car/dashboard/dto/LayoutDto.java
+++ src/main/java/com/globallogic/car/dashboard/dto/LayoutDto.java
@@ -1,5 +1,11 @@
 package com.globallogic.car.dashboard.dto;
 
+import java.io.IOException;
+
+import org.springframework.web.multipart.MultipartFile;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 import lombok.Data;
 
 @Data
@@ -7,5 +13,17 @@ public class LayoutDto {
 
 	private Long layoutId;
 	private String layoutName;
-	private CarDto car;
+	private Long carId;
+	
+	@JsonIgnore
+	private byte[] templatePreview;
+	@JsonIgnore
+	private String templateName;
+	
+	public void readFile(final MultipartFile file) throws IOException {
+		if (file != null) {
+			this.templatePreview = file.getBytes();
+			this.templateName = file.getOriginalFilename();
+		}
+	}
 }
